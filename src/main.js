b49dbda6c234806b995d706a7261a1bda94b73f7
renalfarid <enal.farid@gmail.com>
2024-05-18 02:10:43 +0800

    setup router plugin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { registerPlugins } from '@/plugins'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+registerPlugins(app)
+app.mount('#app')
