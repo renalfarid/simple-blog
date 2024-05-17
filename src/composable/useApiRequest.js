b1d87b6075ab94ec6a274152f86ce91c79af9147
renalfarid <enal.farid@gmail.com>
2024-05-18 01:20:59 +0800

    composable api

---
+++ src/composable/useApiRequest.js
@@ -0,0 +1,32 @@
+import { ref, reactive } from 'vue';
+
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || 'https://blog.test/api';
+
+export default function useApiRequest() {
+  const state = reactive({
+    data: null,
+    error: null,
+    loading: false,
+  });
+
+  const fetchPosts = async () => {
+    state.loading = true;
+    try {
+      const response = await fetch(`${BASE_API_URL}/posts`);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+      state.data = data;
+    } catch (error) {
+      state.error = error;
+    } finally {
+      state.loading = false;
+    }
+  };
+
+  return {
+    state,
+    fetchPosts,
+  };
+}
