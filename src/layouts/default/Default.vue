77a5af5d6aadebb2d203524d58e8329bbdb83850
renalfarid <enal.farid@gmail.com>
2024-05-18 05:07:57 +0800

    public layout

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,6 +1,8 @@
 <script setup>
   import DefaultView from './View.vue'
+  import TopMenu from '../../components/ui/TopMenu.vue'
 </script>
 <template>
+  <TopMenu />
   <DefaultView /> 
 </template>
