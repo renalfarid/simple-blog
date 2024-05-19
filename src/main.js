8e73afd18103ccd24521aeae982e01bfde1beec2
renalfarid <enal.farid@gmail.com>
2024-05-19 22:21:26 +0800

    setup pinia store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { registerPlugins } from '@/plugins'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import './style.css'
 import App from './App.vue'
+const pinia = createPinia()
 
 const app = createApp(App)
+app.use(pinia)
 registerPlugins(app)
 app.mount('#app')
